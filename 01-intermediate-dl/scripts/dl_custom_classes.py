7b4aae9ba65f673e323d72367823c0d943b6726e
Leandro Aguazaco <leandro.aguazaco@outlook.com>
2025-11-10 22:41:05 -0500

    Training and evaluation loop example

--- 01-intermediate-dl/scripts/dl_custom_classes.py
+++ 01-intermediate-dl/scripts/dl_custom_classes.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, TensorDataset, DataLoader
 import torch.nn.functional as F
@@ -18,15 +19,18 @@ class WaterDataset (Dataset):
     def __getitem__(self, index):
         features = self.data[index, :-1]
         label = self.data[index, -1]
-        return features, label
+        return (
+            torch.tensor(features).float(),
+            torch.tensor(label).float()
+        )
 
 class Net(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.layer1 = nn.Linear(9, 16)
-        self.layer2 = nn.Linear(16, 8)
-        self.layer3 = nn.Linear(8, 1)
+        self.layer1 = nn.Linear(9, 16) # (9 + 1) * 16 = 160
+        self.layer2 = nn.Linear(16, 8) # (16 + 1) * 8 = 136
+        self.layer3 = nn.Linear(8, 1) # (8 + 1) * 1 = 9
     
     def forward(self, x):
         x = F.relu(self.layer1(x))
